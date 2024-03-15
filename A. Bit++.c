0c20d839130d860a0943c907268690113fd1013f
Ayoraf Hossain <157395132+AyorafHossain@users.noreply.github.com>
2024-03-15 10:24:10 +0600

    Add files via upload

---
+++ A. Bit++.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,x=0;
+    char s[10];
+    scanf("%d",&n);
+    while(n--)
+    {
+        scanf("%s",s);
+        if(s[1]=='+')
+        {
+            x++;
+        }
+        else
+        {
+            x--;
+        }
+    }
+    printf("%d\n",x);
+    return 0;
+}
