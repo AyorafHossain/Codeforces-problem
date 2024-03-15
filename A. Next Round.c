c8de96bc8f9666e0518ebbf7c3f696923098bff6
Ayoraf Hossain <157395132+AyorafHossain@users.noreply.github.com>
2024-03-15 11:04:26 +0600

    Add files via upload

---
+++ A. Next Round.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,k,i,j,a[100],count=0;
+    scanf("%d %d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>=a[k-1]&&a[i]>0)
+            count++;
+    }
+    printf("%d\n",count);
+    return 0;
+}
